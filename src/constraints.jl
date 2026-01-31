ecdfdfd023f2a1c2759a35df971835fe8b4fcdf9
neal-ks <neal.kesterton@gmail.com>
2026-01-31 15:19:04 +0000

    Refactor of the dynamic constraint evaluation.

--- src/constraints.jl
+++ src/constraints.jl
@@ -14,37 +14,28 @@ end
 function construct_dynamic_constraints(_f, disc_map)
     f_interp, E = integration_parameters(disc_map)
     
-    segment_con_func! = function (defects, state_rate_init, states, controls, t_duration, params)
-        # Evaluate the state rates at points that aern't the first in the segment
-        non_init_state = @view states[2:end]
-        non_init_control = @view controls[2:end]
-        state_rate = vec(_f.(non_init_state, non_init_control, params))
-
-        # The state rate function returns a derivative wrt time, this needs to be converted to the derivatives wrt tau
-        # which is the transformed time to between [-1, 1]
-        pushfirst!(state_rate, state_rate_init)
-        nondim_state_rate = state_rate .* (0.5*t_duration)
-
+    segment_con_func! = function (defects, states, state_rates)
         # Constructing the dynamic constraint system
-        stacked_state_rate = stack(nondim_state_rate, dims=1)
+        stacked_state_rate = stack(state_rates, dims=1)
         stacked_state = stack(states, dims=1)
         constraints = f_interp*stacked_state_rate - E*stacked_state
 
         defects .= vec(constraints)
-        state_rate_init .= nondim_state_rate[end]
     end
+
     t_duration = (disc_map.time.t_final - disc_map.time.t_initial)/disc_map.num_segments
     
     return function dynamic_constraints!(defects, states, controls, params)
         # calculate the initial state rate
-        state_rate_init = vec(_f(states[1], controls[1], params) * 0.5 * t_duration)
-        for i=1:disc_map.num_segments
+        state_rate = @. 0.5*t_duration*_f(states, controls, params)
+        @inbounds for i=1:disc_map.num_segments
             xu_idx = ((i-1)*(disc_map.order-1) + 1):(i*(disc_map.order-1) + 1)
             defect_idx = ((i-1)*(disc_map.order-1) + 1):(i*(disc_map.order-1))
-            seg_states = @view states[xu_idx]
-            seg_controls = @view controls[xu_idx]
-            segment_defects = @view defects[defect_idx]
-            segment_con_func!(segment_defects, state_rate_init, seg_states, seg_controls, t_duration, params) 
+
+            defects_seg = @view defects[defect_idx]
+            states_seg = @view states[xu_idx]
+            state_rates_seg = @view state_rate[xu_idx]
+            segment_con_func!(defects_seg, states_seg, state_rates_seg) 
         end
     end
 end
